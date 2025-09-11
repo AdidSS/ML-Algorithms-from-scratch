28a3d2c47959d31e8649693a014dfb9da071b652
AdidSS <A01710023@tec.mx>
2025-09-11 11:09:51 -0600

    Optimizacion Hiperparametros

--- aprobacion_credito_ml.py
+++ aprobacion_credito_ml.py
@@ -343,3 +343,28 @@ plt.ylabel('Importance')
 plt.title('Feature Importance')
 plt.show()
 print(importances)
+
+#Optimización de hiperparámetros con Grid Search
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+grid_search.fit(X_train, y_train)
+print("Best parameters found: ", grid_search.best_params_)
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.show()
+print(evaluation_report(X_train, X_val, X_test, weights, bias))
+#Plot the ROC curve for the train and test model
+y_train_pred = model.predict_proba(X_train)[:, 1]
+y_test_pred = model.predict_proba(X_test)[:, 1]
+fpr_train, tpr_train, _ = roc_curve(y_train, y_train_pred)
+fpr_test, tpr_test, _ = roc_curve(y_test, y_test_pred)
+plt.plot(fpr_train, tpr_train, label='Train')
+plt.plot(fpr_test, tpr_test, label='Test')
